bad1bceb0e91f18798cfa9c0b8d541dd342aac29
Sebastian Silva <sebastian@fuentelibre.org>
2025-12-04 15:17:57 -0500

    feat: Mejorar la animación de nado y ajustar la rotación del modelo

--- scripts/Spin.gd
+++ scripts/Spin.gd
@@ -2,7 +2,11 @@ extends Spatial
 export var speed: float = 0.7
 
 func _ready():
-	$PilotModel/AnimationPlayer.play("Swim_Idle_Loop")
+	var anim = $PilotModel/AnimationPlayer
+	var swim_idle_anim = anim.get_animation("Swim_Idle_Loop")
+	if swim_idle_anim:
+		swim_idle_anim.loop = true
+		anim.play("Swim_Idle_Loop")
 
 func _process(delta):
-	$PilotModel.rotate_y(speed * delta)
+	$PilotModel.rotate_y(speed * delta * 0.2)
