c29313ecce0d459255f50b5f0987b085bdf3d323
Sebastian Silva <sebastian@fuentelibre.org>
2025-12-04 14:21:13 -0500

    feat: Agregar imagen de inicio y ajustar el menú

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-onready var debug_label: Label = $VBoxContainer/HBoxContainer/ScrollContainer/DebugLabel
 onready var cursor: Sprite = $Cursor
 
 func _ready():
@@ -9,17 +8,6 @@ func _ready():
 		var stream := load("res://assets/music/Orbital Descent.mp3")
 		if stream:
 			AudioManager.play_bgm(stream, -8.0, true)
-
-
-func _unhandled_input(event):
-	# --- DEBUG LABEL ---
-	var text = debug_label.text
-	var lista = Array(text.split("\n"))
-	while lista.size() > 11:
-		lista.pop_front()
-	text = PoolStringArray(lista).join("\n")
-	debug_label.text = text + "\n" + event.as_text()
-	# --------------------
 
 func _on_Start_pressed():
 	get_tree().change_scene("res://scenes/levels/act1/Criogenia.tscn")
--- scripts/Spin.gd
+++ scripts/Spin.gd
@@ -1,5 +1,8 @@
 extends Spatial
 export var speed: float = 0.7
 
+func _ready():
+	$PilotModel/AnimationPlayer.play("Swim_Idle_Loop")
+
 func _process(delta):
 	$PilotModel.rotate_y(speed * delta)
