2adc0b3933e615e097f5e1a720704cacd3857536
Sebastian Silva <sebastian@fuentelibre.org>
2026-01-07 20:15:09 -0500

    Remove unused scripts: AnimationLoopFix.gd and LevelBGM.gd; delete REFACTOR.md for outdated replay input specifications.

--- scripts/LevelBGM.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-export (String) var track_path := "res://assets/music/Rust and Ruin.mp3"
-export (float) var volume_db := -8.0
-export (bool) var loop := true
-
-func _ready() -> void:
-	if typeof(AudioSystem) != TYPE_NIL and AudioSystem:
-		AudioSystem.play_bgm(track_path, 0.0, loop, volume_db)
