40e1ce00f17c67ad79b6b887e50a5333d931296d
Sebastian Silva <sebastian@fuentelibre.org>
2026-01-14 04:44:10 -0500

    New base scene

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -3,13 +3,13 @@ extends Control
 export var enable_touch_buttons := true
 
 onready var cursor: Sprite = find_node("Cursor")
-onready var fade_rect: ColorRect = $CanvasLayer/ColorRect  # Agrega un CanvasLayer > ColorRect negro
+onready var fade_rect: ColorRect = $CanvasLayer/ColorRect # Agrega un CanvasLayer > ColorRect negro
 
 func _ready():
 	find_node("Start").grab_focus()
 
 	# Fade in al cargar
-	fade_rect.color.a = 1.0  # Empieza negro
+	fade_rect.color.a = 1.0 # Empieza negro
 	var tween = create_tween()
 	tween.tween_property(fade_rect, "modulate:a", 0.0, 1.0).from(1.0).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 	# El tween se auto-inicia y auto-libera.
@@ -39,7 +39,7 @@ func _on_Start_pressed():
 
 func _on_fade_out_complete():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # Capturamos el mouse
-	get_tree().change_scene("res://scenes/levels/act1/Criogenia.tscn")
+	get_tree().change_scene("res://core_v2/scenes/BaseTerrace.tscn")
 
 func _on_copilot_pressed():
 	"""Multiplayer split-screen."""
