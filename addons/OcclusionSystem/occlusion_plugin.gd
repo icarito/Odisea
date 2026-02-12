8b01f49ff9eac6be5c864191134f07608add2666
Sebastian Silva <sebastian@fuentelibre.org>
2026-02-12 18:37:10 -0500

    feat: Remove Hunyuan Retargeter and Occlusion System addons

--- addons/OcclusionSystem/occlusion_plugin.gd
+++
@@ -1,9 +0,0 @@
-tool
-extends EditorPlugin
-
-func _enter_tree():
-	var gui = get_editor_interface().get_base_control()
-	add_custom_type("OcclusionArea", "Area", preload("res://core_v2/components/OcclusionZoneV2.gd"), gui.get_icon("Area", "EditorIcons"))
-
-func _exit_tree():
-	remove_custom_type("OcclusionArea")
