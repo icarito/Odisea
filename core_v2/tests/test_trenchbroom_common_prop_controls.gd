72adcdca6475554a6e2dd6f99872aa4f5caa9e8c
Sebastian Silva <sebastian@fuentelibre.org>
2026-05-09 11:17:07 -0500

    Add import configuration for EmergencyBeaconV2_contact texture
    - Created a new import file for EmergencyBeaconV2_contact.png
    - Configured texture parameters including compression settings and flags
    - Set up dependencies for the source and destination files

--- core_v2/tests/test_trenchbroom_common_prop_controls.gd
+++ core_v2/tests/test_trenchbroom_common_prop_controls.gd
@@ -38,6 +38,62 @@ func test_qodot_fgd_prop_and_light_classes_expose_common_controls():
 		assert_bool(body.find("is_interactable(") >= 0).is_true()
 		assert_bool(body.find("enable_shadows(") >= 0).is_true()
 
+func test_trenchbroom_work_light_bounds_are_floor_aligned_z_up():
+	var work_light: Resource = load("res://core_v2/qodot_fgd/props/SciFiWorkLightV2_point_class.tres")
+	var tripod: Resource = load("res://core_v2/qodot_fgd/props/SciFiWorkLightTripodV2_point_class.tres")
+	assert_object(work_light).is_not_null()
+	assert_object(tripod).is_not_null()
+
+	var work_light_size: AABB = work_light.get("meta_properties")["size"]
+	var tripod_size: AABB = tripod.get("meta_properties")["size"]
+
+	assert_vector3(work_light_size.position).is_equal(Vector3(-6, -6, 0))
+	assert_vector3(work_light_size.size).is_equal(Vector3(6, 6, 18))
+	assert_vector3(tripod_size.position).is_equal(Vector3(-13, -13, 0))
+	assert_vector3(tripod_size.size).is_equal(Vector3(13, 13, 38))
+
+	var file := File.new()
+	var err := file.open("res://Qodot.fgd", File.READ)
+	assert_int(err).is_equal(OK)
+	var text := file.get_as_text()
+	file.close()
+
+	assert_bool(text.find("size(-6 -6 0, 6 6 18) = light_work") >= 0).is_true()
+	assert_bool(text.find("size(-13 -13 0, 13 13 38) = light_work_tripod") >= 0).is_true()
+
+func test_trenchbroom_floor_hatch_bounds_match_scene_footprint():
+	var hatch: Resource = load("res://core_v2/qodot_fgd/props/FloorHatch_point_class.tres")
+	assert_object(hatch).is_not_null()
+
+	var hatch_size: AABB = hatch.get("meta_properties")["size"]
+	assert_vector3(hatch_size.position).is_equal(Vector3(-32, -32, -2))
+	assert_vector3(hatch_size.size).is_equal(Vector3(32, 32, 2))
+
+	var file := File.new()
+	var err := file.open("res://Qodot.fgd", File.READ)
+	assert_int(err).is_equal(OK)
+	var text := file.get_as_text()
+	file.close()
+	assert_bool(text.find("size(-32 -32 -2, 32 32 2) = prop_door_floor_hatch") >= 0).is_true()
+
+func test_custom_trenchbroom_point_class_bounds_use_grid_units():
+	var file := File.new()
+	var err := file.open("res://Qodot.fgd", File.READ)
+	assert_int(err).is_equal(OK)
+	var lines := file.get_as_text().split("\n")
+	file.close()
+
+	for raw_line in lines:
+		var line := String(raw_line)
+		var is_custom_point: bool = line.begins_with("@PointClass") and (line.find(" = prop_") >= 0 or line.find(" = light_") >= 0)
+		if not is_custom_point or line.find("size(") < 0:
+			continue
+		var size_start := line.find("size(")
+		var size_end := line.find(")", size_start)
+		assert_bool(size_end > size_start).is_true()
+		var size_text := line.substr(size_start, size_end - size_start + 1)
+		assert_bool(size_text.find(".") == -1).is_true()
+
 func test_forward_interact_proxy_respects_disabled_parent_interaction():
 	var packed: PackedScene = load("res://core_v2/props/scifi_lights/SciFiWorkLightTripodV2.tscn")
 	assert_object(packed).is_not_null()
@@ -56,6 +112,91 @@ func test_forward_interact_proxy_respects_disabled_parent_interaction():
 	assert_bool(prop.is_in_group("interactable")).is_false()
 	assert_bool(proxy.is_in_group("interactable")).is_false()
 
+func test_work_light_interaction_collision_does_not_extend_below_origin():
+	var scene_paths := [
+		"res://core_v2/props/scifi_lights/SciFiWorkLightV2.tscn",
+		"res://core_v2/props/scifi_lights/SciFiWorkLightTripodV2.tscn",
+	]
+
+	for scene_path in scene_paths:
+		var packed: PackedScene = load(String(scene_path))
+		assert_object(packed).is_not_null()
+
+		var prop: Spatial = auto_free(packed.instance())
+		add_child(prop)
+		yield(get_tree(), "idle_frame")
+
+		var body: Spatial = prop.get_node("InteractionBody")
+		var collision_shape: CollisionShape = body.get_node("CollisionShape")
+		assert_object(body).is_not_null()
+		assert_object(collision_shape).is_not_null()
+		assert_object(collision_shape.shape).is_not_null()
+		assert_bool(collision_shape.shape is BoxShape).is_true()
+
+		var shape := collision_shape.shape as BoxShape
+		var bottom_y: float = body.translation.y + collision_shape.translation.y - shape.extents.y
+		assert_float(bottom_y).is_equal_approx(0.0, 0.001)
+
+func test_work_light_tripod_feet_are_floor_aligned():
+	var packed: PackedScene = load("res://core_v2/props/scifi_lights/SciFiWorkLightTripodV2.tscn")
+	assert_object(packed).is_not_null()
+
+	var prop: Spatial = auto_free(packed.instance())
+	add_child(prop)
+	yield(get_tree(), "idle_frame")
+
+	var min_y := INF
+	for path in [
+		"Tripod/Leg1Pivot/Leg/Foot",
+		"Tripod/Leg2Pivot/Leg/Foot",
+		"Tripod/Leg3Pivot/Leg/Foot",
+	]:
+		var foot: CSGBox = prop.get_node(path)
+		assert_object(foot).is_not_null()
+		min_y = min(min_y, _box_min_y(foot, foot.width, foot.height, foot.depth))
+
+	assert_float(min_y).is_equal_approx(0.0, 0.001)
+
+func test_emergency_beacon_origin_is_mounting_back_face():
+	var point_class: Resource = load("res://core_v2/qodot_fgd/props/EmergencyBeaconV2_point_class.tres")
+	assert_object(point_class).is_not_null()
+	var tb_size: AABB = point_class.get("meta_properties")["size"]
+	assert_vector3(tb_size.position).is_equal(Vector3(-4, 0, -4))
+	assert_vector3(tb_size.size).is_equal(Vector3(4, 8, 4))
+
+	var file := File.new()
+	var err := file.open("res://Qodot.fgd", File.READ)
+	assert_int(err).is_equal(OK)
+	var text := file.get_as_text()
+	file.close()
+	assert_bool(text.find("size(-4 0 -4, 4 8 4) = light_emergency_beacon") >= 0).is_true()
+
+	var packed: PackedScene = load("res://core_v2/props/EmergencyBeaconV2.tscn")
+	assert_object(packed).is_not_null()
+
+	var prop: Spatial = auto_free(packed.instance())
+	add_child(prop)
+	yield(get_tree(), "idle_frame")
+
+	var bounds := _mesh_instance_bounds(prop)
+	assert_float(bounds.position.y).is_equal_approx(0.0, 0.001)
+	assert_float(abs(bounds.position.z + bounds.size.z * 0.5)).is_less(0.03)
+
+	var collision_shape: CollisionShape = prop.get_node("CollisionShape")
+	assert_object(collision_shape).is_not_null()
+	assert_bool(collision_shape.shape is CylinderShape).is_true()
+	var cylinder := collision_shape.shape as CylinderShape
+	var collision_back_y: float = collision_shape.translation.y - (cylinder.height * 0.5)
+	var collision_center_z: float = collision_shape.translation.z
+	assert_float(collision_back_y).is_greater_equal(-0.001)
+	assert_float(collision_center_z).is_equal_approx(0.0, 0.001)
+
+	var sfx: AudioStreamPlayer3D = prop.get_node("SFX Alarm")
+	assert_object(sfx).is_not_null()
+	assert_vector3(sfx.translation).is_equal(Vector3.ZERO)
+	assert_int(_count_collision_shapes(sfx)).is_equal(0)
+	assert_int(_count_collision_shapes(prop)).is_equal(1)
+
 func test_baseterrace_work_light_matches_trenchbroom_overrides():
 	var packed: PackedScene = load("res://core_v2/levels/BaseTerrace.tscn")
 	assert_object(packed).is_not_null()
@@ -64,8 +205,9 @@ func test_baseterrace_work_light_matches_trenchbroom_overrides():
 	add_child(scene)
 	yield(get_tree(), "idle_frame")
 
-	var tripod: Node = scene.get_node("Building/QodotMap/entity_9_light_work_tripod")
+	var tripod: Node = scene.find_node("*_light_work_tripod", true, false)
 	assert_object(tripod).is_not_null()
+	assert_float((tripod as Spatial).translation.y).is_equal_approx(0.0, 0.001)
 	assert_bool(tripod.get("is_interactable")).is_false()
 	assert_bool(tripod.is_in_group("interactable")).is_false()
 
@@ -76,6 +218,68 @@ func test_baseterrace_work_light_matches_trenchbroom_overrides():
 	var spot: SpotLight = tripod.get_node("Head/SpotLight")
 	assert_object(spot).is_not_null()
 	assert_bool(spot.shadow_enabled).is_true()
+
+func test_baseterrace_emergency_beacons_are_wall_aligned():
+	var packed: PackedScene = load("res://core_v2/levels/BaseTerrace.tscn")
+	assert_object(packed).is_not_null()
+
+	var scene: Node = auto_free(packed.instance())
+	add_child(scene)
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+
+	var space := (scene as Spatial).get_world().direct_space_state
+	for name in [
+		"entity_6_light_emergency_beacon",
+		"entity_8_light_emergency_beacon",
+	]:
+		var beacon: Spatial = scene.find_node(name, true, false)
+		assert_object(beacon).is_not_null()
+		var back := -beacon.global_transform.basis.y.normalized()
+		var front := -back
+		var from := beacon.global_transform.origin + front * 0.12
+		var to := beacon.global_transform.origin + back * 0.25
+		var hit := space.intersect_ray(from, to, [beacon], 2147483647, true, true)
+		assert_bool(hit.empty()).is_false()
+		var signed_hit: float = (hit.position - beacon.global_transform.origin).dot(back)
+		assert_float(abs(signed_hit)).is_less(0.06)
+
+func test_crio_map_emergency_beacon_z_axis_origin_is_wall_aligned():
+	var qodot_map_script: Script = load("res://addons/qodot/src/nodes/qodot_map.gd")
+	assert_object(qodot_map_script).is_not_null()
+
+	var qodot_map: Spatial = auto_free(Spatial.new())
+	qodot_map.set_script(qodot_map_script)
+	add_child(qodot_map)
+
+	qodot_map.map_file = "res://maps/crio.map"
+	qodot_map.inverse_scale_factor = 16.0
+	qodot_map.entity_fgd = load("res://addons/qodot/game_definitions/fgd/qodot_fgd.tres")
+	qodot_map.base_texture_dir = "res://textures"
+	qodot_map.use_trenchbroom_group_hierarchy = true
+	qodot_map.should_add_children = true
+	qodot_map.should_set_owners = false
+	qodot_map.block_until_complete = true
+	qodot_map.verify_and_build()
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+
+	var beacon: Spatial = qodot_map.find_node("entity_6_light_emergency_beacon", true, false)
+	assert_object(beacon).is_not_null()
+	var back := -beacon.global_transform.basis.y.normalized()
+	var front := -back
+	var space := qodot_map.get_world().direct_space_state
+	var hit := space.intersect_ray(
+		beacon.global_transform.origin + front * 0.12,
+		beacon.global_transform.origin + back * 0.25,
+		[beacon],
+		2147483647,
+		true,
+		true
+	)
+	assert_bool(hit.empty()).is_false()
+	var signed_hit: float = (hit.position - beacon.global_transform.origin).dot(back)
+	assert_float(abs(signed_hit)).is_less(0.06)
 
 func test_qodot_map_restores_point_entities_skipped_by_native_parser():
 	var qodot_map_script: Script = load("res://addons/qodot/src/nodes/qodot_map.gd")
@@ -139,3 +343,57 @@ func _extract_custom_classname(header: String) -> String:
 	if classname.begins_with("prop_") or classname.begins_with("light_"):
 		return classname
 	return ""
+
+func _box_min_y(node: Spatial, width: float, height: float, depth: float) -> float:
+	var extents := Vector3(width, height, depth) * 0.5
+	var min_y := INF
+	for x in [-extents.x, extents.x]:
+		for y in [-extents.y, extents.y]:
+			for z in [-extents.z, extents.z]:
+				var point: Vector3 = node.global_transform.xform(Vector3(x, y, z))
+				min_y = min(min_y, point.y)
+	return min_y
+
+func _mesh_instance_bounds(root: Node) -> AABB:
+	var found := false
+	var bounds := AABB()
+	var stack := [root]
+	while not stack.empty():
+		var node: Node = stack.pop_back()
+		if node is MeshInstance:
+			var visual := node as MeshInstance
+			var visual_bounds := _transform_aabb(visual.global_transform, visual.get_aabb())
+			if found:
+				bounds = bounds.merge(visual_bounds)
+			else:
+				bounds = visual_bounds
+				found = true
+		for child in node.get_children():
+			stack.append(child)
+	return bounds
+
+func _count_collision_shapes(root: Node) -> int:
+	var total := 0
+	var stack := [root]
+	while not stack.empty():
+		var node: Node = stack.pop_back()
+		if node is CollisionShape:
+			total += 1
+		for child in node.get_children():
+			stack.append(child)
+	return total
+
+func _transform_aabb(xform: Transform, aabb: AABB) -> AABB:
+	var min_point := Vector3(INF, INF, INF)
+	var max_point := Vector3(-INF, -INF, -INF)
+	for x in [aabb.position.x, aabb.position.x + aabb.size.x]:
+		for y in [aabb.position.y, aabb.position.y + aabb.size.y]:
+			for z in [aabb.position.z, aabb.position.z + aabb.size.z]:
+				var point: Vector3 = xform.xform(Vector3(x, y, z))
+				min_point.x = min(min_point.x, point.x)
+				min_point.y = min(min_point.y, point.y)
+				min_point.z = min(min_point.z, point.z)
+				max_point.x = max(max_point.x, point.x)
+				max_point.y = max(max_point.y, point.y)
+				max_point.z = max(max_point.z, point.z)
+	return AABB(min_point, max_point - min_point)
