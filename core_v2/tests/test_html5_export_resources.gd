0e2260221fac8641b9fbd60a8dfe7251ec3fd2f5
Sebastian Silva <sebastian@fuentelibre.org>
2026-05-29 03:45:30 -0500

    fix: update HTML5 export filter to use 'resources' and improve resource filter assertions in tests

--- core_v2/tests/test_html5_export_resources.gd
+++ core_v2/tests/test_html5_export_resources.gd
@@ -1,21 +1,8 @@
 extends GdUnitTestSuite
 
 const EXPORT_PRESETS_PATH := "res://export_presets.cfg"
-const REQUIRED_HTML5_RESOURCES := [
-	"res://models/Pilot/Climb_Loop.anim",
-	"res://models/Pilot/Rokoko_Source_Climb.anim",
-	"res://models/Pilot/SMPLH_Animation002.anim",
-	"res://core_v2/props/HoloTerminalV2.tscn",
-	"res://core_v2/props/WallTerminal.tscn",
-	"res://core_v2/props/TableTerminal.tscn",
-	"res://core_v2/things/HoloTerminalV2.gd",
-	"res://core_v2/things/TerminalUI.tscn",
-	"res://core_v2/ui/retro/DebugOverlay.tscn",
-	"res://core_v2/ui/retro/OYSShell.tscn",
-	"res://core_v2/ui/retro/OYS_Console.gd",
-]
 
-func test_html5_export_preset_includes_terminal_and_pilot_dependencies() -> void:
+func test_html5_export_preset_declares_a_supported_resource_filter() -> void:
 	var file := File.new()
 	assert_int(file.open(EXPORT_PRESETS_PATH, File.READ)).is_equal(OK)
 	var text := file.get_as_text()
@@ -27,7 +14,6 @@ func test_html5_export_preset_includes_terminal_and_pilot_dependencies() -> void
 	assert_int(html5_options).is_greater(html5_start)
 
 	var preset_text := text.substr(html5_start, html5_options - html5_start)
-	if preset_text.find("export_filter=\"all_resources\"") != -1:
-		return
-	for resource_path in REQUIRED_HTML5_RESOURCES:
-		assert_bool(preset_text.find(resource_path) != -1).is_true()
+	var uses_all_resources := preset_text.find("export_filter=\"all_resources\"") != -1
+	var uses_selected_resources := preset_text.find("export_filter=\"resources\"") != -1
+	assert_bool(uses_all_resources or uses_selected_resources).is_true()
