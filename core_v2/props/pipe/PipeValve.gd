713ae8a05dd2e56db1026b18b58ded1609fb44f0
Sebastian Silva <sebastian@fuentelibre.org>
2026-09-02 22:59:30 -0500

    Keep status emitters below bloom threshold

--- core_v2/props/pipe/PipeValve.gd
+++ core_v2/props/pipe/PipeValve.gd
@@ -88,7 +88,7 @@ func _update_visuals() -> void:
 			mat.albedo_color = color
 			mat.emission = color
 			mat.emission_enabled = true
-			mat.emission_energy = 1.0
+			mat.emission_energy = 0.5
 
 func _on_animation_completed() -> void:
 	._on_animation_completed()
